f993fc3711a81aecf3e08c55ea7aae695d5c7493
wlodarczakm <m.wlodarczak.it@protonmail.com>
2023-02-12 14:24:28 +0100

    add test page users

--- src/main/java/webpages/MyAccountPage.java
+++ src/main/java/webpages/MyAccountPage.java
@@ -1,4 +1,75 @@
 package webpages;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.Users;
+import java.util.ArrayList;
+import java.util.List;
 public class MyAccountPage {
+    WebDriver driver;
+    @FindBy(css = "input[id='username']")
+    WebElement usernameInput;
+
+    @FindBy(css = "input[id='password']")
+    WebElement passwordInput;
+
+    @FindBy(css = "button[class*='submit']")
+    WebElement submitButton;
+
+    @FindBy(css = "ul[class='woocommerce-error'] li")
+
+    WebElement loginErrorMessage;
+    @FindBy(css = "li[class*='is-active'] a")
+    WebElement dashboardButton;
+    @FindBy(css = "li[class*='woocommerce-MyAccount-navigation-link--orders'] a")
+    WebElement ordersButton;
+    @FindBy(css = "li[class*='woocommerce-MyAccount-navigation-link--edit-address'] a")
+    WebElement addressesButton;
+    @FindBy(css = "li[class*='woocommerce-MyAccount-navigation-link--edit-account'] a")
+    WebElement editAccountButton;
+    @FindBy(css = "li[class*='woocommerce-MyAccount-navigation-link--customer-logout'] a")
+    WebElement logoutButton;
+
+    @FindBy(className = "woocommerce-notices-wrapper")
+    WebElement helloMessage;
+
+    public MyAccountPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+    public void FillInputs(Users user) {
+        usernameInput.sendKeys(user.username);
+        passwordInput.sendKeys(user.password);
+    }
+    public void login() {
+        submitButton.click();
+    }
+
+    public String errorMessageText() {
+        return loginErrorMessage.getText();
+    }
+    public WebElement getLogoutButton() {return logoutButton;
+    }
+    public String passwordInputIsEmptyMessage() {
+        return "pole hasła jest puste.";
+    }
+    public String usernameInputIsRequiredMessage() {
+        return "Nazwa użytkownika jest wymagana.";
+    }
+    public String wrongUserMessage() {
+        return "wśród zarejestrowanych w witrynie użytkowników." +
+                " Jeśli nie masz pewności co do nazwy użytkownika, użyj adresu e-mail.";
+    }
+    public List<WebElement> DashboardElements() {
+        List<WebElement> allDashboardElements = new ArrayList<>();
+        allDashboardElements.add(dashboardButton);
+        allDashboardElements.add(ordersButton);
+        allDashboardElements.add(addressesButton);
+        allDashboardElements.add(editAccountButton);
+        allDashboardElements.add(logoutButton);
+        return allDashboardElements;
+    }
 }
+
+
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,2 +1,64 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import webpages.MyAccountPage;
+import utils.Users;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 public class LoginTests {
+    WebDriver driver = new ChromeDriver();
+    MyAccountPage loginPage;
+    List<WebElement> elements;
+    private MyAccountPage elementsAfterLogin;
+    @BeforeEach
+    void prepareBrowser() {
+        driver.manage().window().maximize();
+        driver.get("http://serwer169007.lh.pl/autoinstalator/serwer169007.lh.pl/wordpress10772/moje-konto/");
+        loginPage = new MyAccountPage(driver);
+        elementsAfterLogin = new MyAccountPage(driver);
+    }
+    @AfterEach
+    void closeBrowser() {
+        driver.quit();
+    }
+    @Test
+    void login_when_credentials_are_correct() {
+        loginPage.FillInputs(Users.registeredUser);
+        loginPage.login();
+        assertTrue(loginPage.getLogoutButton().isDisplayed());
+    }
+    @Test
+    void displays_dashboard_when_registered_user_login() {
+        loginPage.FillInputs(Users.registeredUser);
+        loginPage.login();
+
+        elements = elementsAfterLogin.DashboardElements();
+        for (WebElement element : elements) {
+            assertTrue(element.isDisplayed());
+        }
+    }
+    @Test
+    void appear_error_message_when_only_password_given() {
+        loginPage.FillInputs(Users.onlyPasswordGivenUser);
+        loginPage.login();
+
+        assertTrue(loginPage.errorMessageText().contains(loginPage.usernameInputIsRequiredMessage()));
+    }
+    @Test
+    void appear_error_message_when_only_username_given() {
+        loginPage.FillInputs(Users.onlyUsernameGivenUser);
+        loginPage.login();
+
+        assertTrue(loginPage.errorMessageText().contains(loginPage.passwordInputIsEmptyMessage()));
+    }
+    @Test
+    void appear_error_message_when_not_registered_user_try_login() {
+        loginPage.FillInputs(Users.randomUser);
+        loginPage.login();
+
+        assertTrue(loginPage.errorMessageText().contains(loginPage.wrongUserMessage()));
+    }
 }
+
+
