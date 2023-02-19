147b818dda6b137cfe9123dc92f6cfdd628b704f
wlodarczakm <m.wlodarczak.it@protonmail.com>
2023-02-19 18:50:53 +0100

    .

--- src/main/java/webpages/ContactPage.java
+++ src/main/java/webpages/ContactPage.java
@@ -1,4 +1,15 @@
 package webpages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class ContactPage {
+    WebDriver driver;
+    @FindBy(css = )
+
+    public ContactPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 }
