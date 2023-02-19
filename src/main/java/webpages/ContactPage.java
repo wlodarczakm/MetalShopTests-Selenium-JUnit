6b9c726df7aff395dd80c19859e82b1d97de369e
wlodarczakm <m.wlodarczak.it@protonmail.com>
2023-02-19 18:59:40 +0100

    new WebElements

--- src/main/java/webpages/ContactPage.java
+++ src/main/java/webpages/ContactPage.java
@@ -1,13 +1,20 @@
 package webpages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ContactPage {
     WebDriver driver;
-    @FindBy(css = )
-
+    @FindBy(css ="input[name='your-name']")
+    WebElement fullNameInput;
+    @FindBy(css = "input[type='email']")
+    WebElement emailInput;
+    @FindBy(css = "input[name*='subject']")
+    WebElement subjectInput;
+    @FindBy(css = "textarea[class='wpcf7-form-control wpcf7-textarea']")
+    WebElement messageInput;
     public ContactPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
