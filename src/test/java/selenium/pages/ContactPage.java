628b91f60148f8e17f009354f745c942a3aa6264
wlodarczakm <m.wlodarczak.it@protonmail.com>
2023-06-10 14:32:57 +0200

    .

--- src/test/java/selenium/pages/ContactPage.java
+++ src/test/java/selenium/pages/ContactPage.java
@@ -2,18 +2,19 @@ package selenium.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.seleniumhq.jetty9.server.Authentication;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import selenium.utils.Users;
-
-import java.util.concurrent.TimeUnit;
-
-import static selenium.utils.Users.*;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactPage {
     WebDriver driver;
+    Users user;
+
     @FindBy(css = "input[name='your-name']")
     WebElement fullNameInput;
     @FindBy(css = "input[type='email']")
@@ -24,18 +25,52 @@ public class ContactPage {
     WebElement messageInput;
     @FindBy(css = "input[type='submit']")
     WebElement submit;
+
+    @FindBy(css = "div[class='wpcf7-response-output']")
+    WebElement sendContactMessageError;
+
+
+
     public ContactPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
-    public void FillInput(Users user) {
+
+    public List<WebElement> ContactFormElements() {
+        List<WebElement> allDashboardElements = new ArrayList<>();
+        allDashboardElements.add(fullNameInput);
+        allDashboardElements.add(emailInput);
+        allDashboardElements.add(subjectInput);
+        allDashboardElements.add(messageInput);
+        return allDashboardElements;
+
+    }
+
+    public void WaitClass() {
+        WebDriverWait WAIT = new WebDriverWait(driver, Duration.ofSeconds(1));
+        for (WebElement element : ContactFormElements()) {
+            WAIT.until(ExpectedConditions.visibilityOfAllElements(element));
+        }
+    }
+    public void FillInput() {
+        user = new Users();
+
         fullNameInput.sendKeys(user.fullName);
         emailInput.sendKeys(user.email);
         subjectInput.sendKeys(user.subject);
         messageInput.sendKeys(user.message);
     }
+
+    public void WaitForErroMessage() {
+        WebDriverWait WAIT = new WebDriverWait(driver, Duration.ofSeconds(1));
+        WAIT.until(ExpectedConditions.visibilityOf(sendContactMessageError));
+    }
     public void submitContactForm(){
         submit.click();
     }
 
+    public boolean AppearErrorWhenSendingMessage() {
+        return sendContactMessageError.getText().contains
+            ("Wystąpił problem z wysłaniem twojej wiadomości. Spróbuj ponownie później.");
+    }
 }
--- src/test/java/selenium/test/ContactPageTests.java
+++ src/test/java/selenium/test/ContactPageTests.java
@@ -3,35 +3,27 @@ package selenium.test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import selenium.pages.ContactPage;
-import selenium.utils.Users;
-
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ContactPageTests {
-    WebDriver driver = new ChromeDriver();
+    WebDriver driver;
     ContactPage contactPage;
-    Wait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    ContactPage contactFormElements;
 
     @BeforeEach
     void prepareBrowser() {
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
-//        WebDriver driver = new ChromeDriver(options);
+//        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+//        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
         driver.get("http://serwer169007.lh.pl/autoinstalator/serwer169007.lh.pl/wordpress10772/kontakt/");
         contactPage = new ContactPage(driver);
-//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
+        contactFormElements = new ContactPage(driver);
     }
     @AfterEach
     void closeBrowser() {
@@ -40,9 +32,10 @@ public class ContactPageTests {
 
     @Test
     void ContactMessage() {
-        contactPage.FillInput(new Users());
+        contactPage.FillInput();
         contactPage.submitContactForm();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//div[contains(@class, 'wpcf7-response-output')]")));
-        assertTrue(driver.findElement(By.xpath(".//div[contains(@class, 'wpcf7-response-output')]")).getText().contains("Wystąpił problem z wysłaniem twojej wiadomości. Spróbuj ponownie później."));
+        contactPage.WaitForErroMessage();
+
+        assertTrue(contactPage.AppearErrorWhenSendingMessage());
     }
 }
