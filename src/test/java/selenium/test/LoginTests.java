e8fdb95fd8c3ec85fe2ac3875180a30d73c72f28
wlodarczakm <m.wlodarczak.it@protonmail.com>
2023-06-11 20:46:41 +0200

    .

--- src/test/java/selenium/test/LoginTests.java
+++ src/test/java/selenium/test/LoginTests.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import selenium.pages.HelperClaseeTest;
 import selenium.pages.MyAccountPage;
 import java.util.List;
 
